57bfce6639f551b53c9f97f7a1e540dd43e74ebf
Skorban <skorban@outlook.com>
2021-03-26 13:50:39 +0100

    Initial Commit

---
+++ First/FirstApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstApp.swift
+//  First
+//
+//  Created by Paweł Ładna on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
